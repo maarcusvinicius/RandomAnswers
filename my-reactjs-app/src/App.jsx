e3cdd1b1572325f097d756fc9cfb90e928f5bab8
maarcusvinicius <marcus.editor77@gmail.com>
2022-08-29 10:29:59 -0300

    tentation 1 adptation reactjs in proj

--- my-reactjs-app/src/App.jsx
+++ my-reactjs-app/src/App.jsx
@@ -1,19 +1,27 @@
-import { fazerPergunta } from './Components/FazerPer';
-import './App.css'
+import { fazerPergunta } from '../src/Components/FazerPer';
+import './App.css';
 
 function App() {
+
+  const elementoResposta = document.querySelector('#resposta')
+  const inputPergunta = document.querySelector("#inputPergunta")
+  const buttonPerguntar = document.querySelector('#buttonPerguntar')
+  const respostas = [
+    "PEgouuuuuuuuuu!"
+  ]
+
   return (
-    <div id="container">
+    <div className="container">
       <img
         src="https://static.wixstatic.com/media/5fb0d1_159e06a06085497ab368ce408a7d9661~mv2.gif"
         alt="SpaceExploration"
       />
       <h1>Vou revelar seu destino!</h1>
       <p>Clique em fazer pergunta para que seu destino seja revelado.</p>
-      <input id="inputPergunta" value="" type="text" placeholder="Digite sua pergunta" />
-      <button id="buttonPerguntar" onClick={fazerPergunta()}>Fazer Pergunta</button>
+      <input className="inputPergunta" type="text" placeholder="Digite sua pergunta" />
+      <button className="buttonPerguntar" type="button" onClick={fazerPergunta}>Fazer Pergunta</button>
 
-      <h3 id="resposta"></h3>
+      <h3 className="resposta"></h3>
     </div>
   )
 }
